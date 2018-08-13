f633afe21d4f5e3d6e3709b63979f36a4ea7434d
Kryndach <kryndach.ios@gmail.com>
2018-08-13 13:45:38 +0600

    added App class

---
+++ app/src/main/java/io/blackbricks/cleanandroidtemplate/App.java
@@ -0,0 +1,12 @@
+package io.blackbricks.cleanandroidtemplate;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
